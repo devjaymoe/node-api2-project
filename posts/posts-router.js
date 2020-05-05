4d899a96c95c679667978a288382fcaab91ede99
Devin_Moore <d.j.moore209@gmail.com>
2020-05-05 18:44:03 -0400

    all crud ops completed

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -50,9 +50,55 @@ router.get('/:id/comments', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     Posts.findById(req.params.id)
+        .then(post => {
+            if (post) {
+
+                Posts.remove(req.params.id)
+                .then(response => {
+                    res.status(200).json(post)
+                })
+                .catch(err => {
+                    console.log(err)
+                    res.status(500).json({
+                        message: 'there was an error deleting the posts'
+                    });
+                });
+
+            } else if (!post) {
+                res.status(200).json({message: 'Post not found'})
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: 'error retrieving the posts'
+            });
+        });
+});
+
+router.post('/', (req, res) => {
+    
+    Posts.insert(req.body)
+        .then(response => {
+            res.status(201).json(response)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({
+                message: 'Error Adding the post'
+            });
+        });
+});
+
+router.post('/:id/comments', (req, res) => {
+
+    Posts.findPostComments(req.params.id)
         .then(posts => {
             if (posts) {
-                res.status(200).json(posts)
+                Posts.insertComment(req.body)
+                .then(response => {
+                    res.status(201).json(response)
+                })
             } else {
                 res.status(200).json({message: 'Post not found'})
             }
@@ -65,6 +111,26 @@ router.delete('/:id', (req, res) => {
         });
 });
 
+router.put('/:id', (req, res) => {
+    Posts.update(req.params.id, req.body)
+        .then( response => {
+            console.log(response)
+            Posts.findById(req.params.id)
+                .then(post => {
+                    res.status(200).json(post)
+                })
+                .catch(error => {
+                    console.log(error)
+                    res.status(200).json({
+                        message: 'there was an error finding the post id'
+                    });
+                });
+        })
+        .catch( error => {
+            console.log(error)
+            res.status(404).json({ message: 'post not found'});
+        });
+});
 
 
 module.exports = router;
