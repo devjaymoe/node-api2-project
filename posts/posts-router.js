c40b730cce4df1a5d7050bcca5fee033cc4c0f8a
Devin_Moore <d.j.moore209@gmail.com>
2020-05-05 21:19:19 -0400

    error handling for comments

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,12 +4,13 @@ const Posts = require('../data/db.js');
 router.get('/', (req, res) => {
     Posts.find(req.query)
         .then(posts => {
+            console.log(posts)
             res.status(200).json(posts)
         })
         .catch(err => {
             console.log(err)
             res.status(500).json({
-                message: 'error retrieving the posts'
+                message: 'The posts information could not be retrieved.'
             });
         });
 });
@@ -17,6 +18,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     Posts.findById(req.params.id)
         .then(posts => {
+            console.log(posts)
             if (posts) {
                 res.status(200).json(posts)
             } else {
@@ -77,31 +79,93 @@ router.delete('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    const blogPost = req.body
     
-    Posts.insert(req.body)
-        .then(response => {
-            res.status(201).json(response)
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(500).json({
-                message: 'Error Adding the post'
+    if (blogPost.title && blogPost.contents) {
+
+        Posts.insert(blogPost)
+            .then(response => {
+                const newId = response.id
+                return newId
+            })
+            .then(newId =>{
+
+                Posts.findById(newId)
+                    .then(post => {
+                        res.status(201).json(post)
+                    })
+                    .catch(error => console.log(error))
+
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(500).json({
+                    message: 'There was an error while saving the post to the database'
+                });
             });
-        });
+
+    } else {
+
+        res.status(400).json({
+            message: "Please provide a title and content to the post"
+        })
+
+    }
+
 });
 
 router.post('/:id/comments', (req, res) => {
+    const id = req.params.id
+    const commentReq = req.body
 
-    Posts.findPostComments(req.params.id)
-        .then(posts => {
-            if (posts) {
-                Posts.insertComment(req.body)
-                .then(response => {
-                    res.status(201).json(response)
-                })
+    Posts.findPostComments(id)
+        .then(comment => {
+
+            const [commentObj] = comment
+
+            if (commentObj) {
+
+                if (commentReq.text && typeof commentReq.text === 'string'){
+
+                    Posts.insertComment(commentReq)
+                    .then(newId => {
+                        const DummyThickId = newId.id
+                        return DummyThickId
+                    })
+                    .then(DummyThickId => {
+
+                        Posts.findCommentById(DummyThickId)
+                            .then(comment => {
+
+                                res.status(201).json(comment)
+                            })
+                            .catch(err => {
+
+                                res.status(201).json({ 
+                                    error: 'Couldnt find comment'
+                                })
+
+                            })
+
+                    })
+                    .catch( err => {
+                        console.log(err)
+                        res.status(500).json({
+                            error: "There was an error while saving the comment to the database"
+                        })
+                    })
+
+                } else {
+                    
+                    res.status(400).json({
+                        message: 'Please provide text for the comment.'
+                    })
+
+                }
             } else {
-                res.status(200).json({message: 'Post not found'})
+                res.status(404).json({message: 'The post with the specified ID does not exist.'})
             }
+
         })
         .catch(err => {
             console.log(err)
